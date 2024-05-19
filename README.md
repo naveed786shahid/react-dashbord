8eddc596c548776fa04db0cab2f6c5383eaebb61
Naveed <naveed786shahid@gmail.com>
2024-05-19 09:50:07 +0300

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+Run in terminal this command:
+
+```bash
+npm install
+```
+
+Then run this command to start your local server
+
+```bash
+npm start
+```
---
+++ src/views/admin/default/variables/columnsData.js
@@ -0,0 +1,36 @@
+export const columnsDataCheck = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "Company",
+    accessor: "company",
+  },
+  {
+    Header: "Username",
+    accessor: "username",
+  },
+  {
+    Header: "Email",
+    accessor: "email",
+  },
+];
+export const columnsDataComplex = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "STATUS",
+    accessor: "status",
+  },
+  {
+    Header: "DATE",
+    accessor: "date",
+  },
+  {
+    Header: "PROGRESS",
+    accessor: "progress",
+  },
+];
